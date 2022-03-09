09ed99eb3e2505eefd60cee2fdcd022224f81c95
jaime <riverac85@gmail.com>
2022-03-09 11:34:38 -0500

    Chapter 27 - Step 12 - Crud local storage

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -4,23 +4,7 @@ import 'package:path/path.dart' show join;
 import 'package:path_provider/path_provider.dart'
     show MissingPlatformDirectoryException, getApplicationDocumentsDirectory;
 
-class DatabaseAlreadyOpenException implements Exception {}
-
-class UnableToGetDocumentsDirectoryException implements Exception {}
-
-class DatabaseIsNotOpenException implements Exception {}
-
-class CouldNotDeleteUserException implements Exception {}
-
-class CouldNotDeleteNoteException implements Exception {}
-
-class CouldNotUpdateNoteException implements Exception {}
-
-class UserAlreadyExistsException implements Exception {}
-
-class CouldNotFindUserException implements Exception {}
-
-class CouldNotFindNoteException implements Exception {}
+import 'crud_exceptions.dart';
 
 class NotesService {
   Database? _db;
